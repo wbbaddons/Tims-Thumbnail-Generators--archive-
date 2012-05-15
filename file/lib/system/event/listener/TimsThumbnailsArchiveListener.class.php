65c229463892b411a3e1c3761cb855fcb7b681e1
Tim Düsterhus <timwolla@arcor.de>
2012-05-15 21:28:44 +0200

    Initial commit.

---
+++ file/lib/system/event/listener/TimsThumbnailsArchiveListener.class.php
@@ -0,0 +1,112 @@
+<?php
+namespace wcf\system\event\listener;
+
+/**
+ * Generates thumbnails for archives.
+ *
+ * @author 	Tim Düsterhus
+ * @copyright	2012 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	be.bastelstu.wcf.thumbnailGenerators.archive
+ * @subpackage	system.event.listener
+ */
+class TimsThumbnailsArchiveListener implements \wcf\system\event\IEventListener {
+	private $eventObj = null;
+	
+	/**
+	 * @see	\wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		$this->eventObj = $eventObj;
+		switch ($eventName) {
+			case 'checkThumbnail':
+			case 'generateThumbnail':
+				$this->$eventName();
+			default:
+				return;
+		}
+	}
+	
+	/**
+	 * Registers the files for thumbnail-creation
+	 */
+	public function checkThumbnail() {
+		switch ($this->eventObj->eventAttachment->fileType) {
+			case 'application/zip':
+			case 'application/x-tar':
+				$this->eventObj->eventData['hasThumbnail'] = true;
+		}
+	}
+	
+	/**
+	 * Actually generate the thumbnail.
+	 */
+	public function generateThumbnail() {
+		switch ($this->eventObj->eventAttachment->fileType) {
+			case 'application/zip':
+			case 'application/x-tar':
+			break;
+			default:
+				return;
+		}
+		
+		// load data
+		$tinyAdapter = \wcf\system\image\ImageHandler::getInstance()->getAdapter();
+		$adapter = \wcf\system\image\ImageHandler::getInstance()->getAdapter();
+		switch ($this->eventObj->eventAttachment->fileType) {
+			case 'application/zip':
+				$file = new \wcf\system\io\Zip($this->eventObj->eventAttachment->getLocation());
+				$file->jumpToCentralDirectory();
+				$centralDirectory = $file->readCentralDirectory();
+				$files = $centralDirectory['files'];
+			break;
+			case 'application/x-tar':
+				$file = new \wcf\system\io\Tar($this->eventObj->eventAttachment->getLocation());
+				$files = $file->getContentList();
+			break;
+		}
+		
+		// plug the sheeps onto the wooden sticks and submerge them in the color
+		$tinyAdapter->createEmptyImage(144, 144);
+		$adapter->createEmptyImage(ATTACHMENT_THUMBNAIL_WIDTH, ATTACHMENT_THUMBNAIL_HEIGHT);
+		$tinyAdapter->setColor(0x00, 0x00, 0x00);
+		$adapter->setColor(0x00, 0x00, 0x00);
+		
+		$i = 1;
+		foreach ($files as $file) {
+			// tabs cannot be displayed with gdlib
+			$file = str_replace("\t", "    ", $file['filename']);
+			
+			$tinyAdapter->drawText($file, 5, $i * 10);
+			$adapter->drawText($file, 5, $i * 10);
+			$i++;
+		}
+		
+		// and create the images
+		$tinyThumbnailLocation = $this->eventObj->eventAttachment->getTinyThumbnailLocation();
+		$thumbnailLocation = $this->eventObj->eventAttachment->getThumbnailLocation();
+		
+		$tinyAdapter->writeImage($tinyThumbnailLocation.'.png');
+		rename($tinyThumbnailLocation.'.png', $tinyThumbnailLocation);
+		$adapter->writeImage($thumbnailLocation.'.png');
+		rename($thumbnailLocation.'.png', $thumbnailLocation);
+		
+		// calculate the thumbnail data
+		$updateData = array();
+		if (file_exists($tinyThumbnailLocation) && ($imageData = @getImageSize($tinyThumbnailLocation)) !== false) {
+			$updateData['tinyThumbnailType'] = $imageData['mime'];
+			$updateData['tinyThumbnailSize'] = @filesize($tinyThumbnailLocation);
+			$updateData['tinyThumbnailWidth'] = $imageData[0];
+			$updateData['tinyThumbnailHeight'] = $imageData[1];
+		}
+		
+		if (file_exists($thumbnailLocation) && ($imageData = @getImageSize($thumbnailLocation)) !== false) {
+			$updateData['thumbnailType'] = $imageData['mime'];
+			$updateData['thumbnailSize'] = @filesize($thumbnailLocation);
+			$updateData['thumbnailWidth'] = $imageData[0];
+			$updateData['thumbnailHeight'] = $imageData[1];
+		}
+		
+		$this->eventObj->eventData = $updateData;
+	}
+}
---
+++ packageBuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+[ -z $PACKAGE_NAME ] && exit 1
+test -e acptemplates && echo "\nBuilding acptemplates.tar\n-------------------------" && cd acptemplates && tar cvf ../acptemplates.tar * && cd ..
+test -e files && echo "\nBuilding files.tar\n------------------" && cd files && tar cvf ../files.tar * && cd ..
+test -e templates && echo "\nBuilding templates.tar\n----------------------" && cd templates && tar cvf ../templates.tar * && cd ..
+test -e acptemplate && echo "\nBuilding acptemplate.tar\n-------------------------" && cd acptemplate && tar cvf ../acptemplate.tar * && cd ..
+test -e file && echo "\nBuilding file.tar\n------------------" && cd file && tar cvf ../file.tar * && cd ..
+test -e template && echo "\nBuilding template.tar\n----------------------" && cd template && tar cvf ../template.tar * && cd ..
+
+
+echo "\nBuilding $PACKAGE_NAME.tar"
+echo -n "-------------"
+for i in `seq 1 ${#PACKAGE_NAME}`;
+do
+		echo -n "-"
+done
+echo -n "\n"
+tar cvf $PACKAGE_NAME.tar * --exclude acptemplates --exclude files --exclude templates --exclude acptemplate --exclude file --exclude template
+
+test -e acptemplates.tar && rm acptemplates.tar
+test -e files.tar && rm files.tar
+test -e templates.tar && rm templates.tar
+test -e acptemplate.tar && rm acptemplate.tar
+test -e file.tar && rm file.tar
+test -e template.tar && rm template.tar
+
+
+exit 0
